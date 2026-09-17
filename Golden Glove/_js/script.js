f3409ba9101a92bd13c1abc49b8f291d157bdff6
winter1112 <nomondemakhushe2@gmail.com>
2026-09-17 15:47:34 +0200

    Add files via upload

---
+++ Golden Glove/_js/script.js
@@ -0,0 +1,9 @@
+// Welcome message
+window.onload = function() {
+    console.log("Welcome to GoldenGlove Jewellery!");
+};
+
+// Enquiry form
+function submitEnquiry() {
+    alert("Thank you for your enquiry! We will contact you soon.");
+}
